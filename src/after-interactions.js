de069121e09b576c110ca505d9675c6d032bf000
Jeff Hanson <hanson@uber.com>
2017-07-08 10:53:14 -0700

    add prop-types, yarn

--- src/after-interactions.js
+++ src/after-interactions.js
@@ -1,4 +1,5 @@
-import {Component, PropTypes} from 'react';
+import PropTypes from 'prop-types';
+import {Component} from 'react';
 import {InteractionManager} from 'react-native';
 
 export default class AfterInteractions extends Component {
